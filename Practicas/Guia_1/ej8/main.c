125569d37cece7168e0f62395f5b8b6cc3c5918a
MartinCodecgg <definit04@gmail.com>
2025-08-26 18:24:33 -0300

    Create main.c

---
+++ Practicas/Guia_1/ej8/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a = 0;
+    if(a == 0) {
+        a = 1;
+        printf("a vale 0 \n");
+    }
+    else
+        printf("a vale 1");
+
+    printf("%d",a);
+}
