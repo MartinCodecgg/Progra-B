0b8f8498c922bcc4cf914f3b2df6c5dbdc813f68
MartinCodecgg <definit04@gmail.com>
2025-09-28 19:52:46 -0300

    Puto el que LEE

--- Parciales/Primer Parcial/README.md
+++ Parciales/Primer Parcial/README.md
@@ -1,1 +1,1 @@
-## 👉Empiezen por el 2019-10 y luego del 2023-06 en adelante y no sean giles
+## 👉Empiecen por el 2019-10 y luego hagan los **parciales** del 2023-06 en adelante y no sean giles
--- Practicas/Guia_6/ej11/main.c
+++ Practicas/Guia_6/ej11/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+void lc_eliminaelem(TLISTAC *pult, TELEMENTOLC elem, int *eliminado) { //nombre de funcion invalido, no puede comenzar con numero
+ TLISTAC ant, act = *pult->sig;
+ eliminado = 0;
+ if (pult = NULL) {
+ do{
+ ant = act;
+ act->sig = act;
+ }while ( *pult != act && elem < act.dato);
+ if (elem = act->dato){
+ if (*pult != (*pult)->sig)
+ *pult= NULL;
+ else {
+ act->sig = ant->sig;
+ if (act == *pult)
+ pult = *ant;
+ }
+ free (ant);
+ eliminado = 1;
+ }
+ }
+}
+
+
+int main() {
+
+
+
     return 0;
 }
