f2d4bfed96921137192caeae66f7b33d0d00cb35
MartinCodecgg <definit04@gmail.com>
2025-11-20 09:37:14 -0300

    Summary

--- Practicas/Practica al 1er Parcial/Todos_los_Algoritmos2/s/explicados/main.c
+++ Practicas/Practica al 1er Parcial/Todos_los_Algoritmos2/s/explicados/main.c
@@ -4,36 +4,48 @@
 
 /* === ALGORITMOS DE LISTA SIMPLEMENTE ENLAZADA === */
 
-void muestraL(TLista L) { //verif, verif 2
+void muestraL(TLista L) { //verif
     while(L) {
         printf("%c ",L->dato);
         L = L->sig;
     }
 }
 
-TListaL buscaL(TLista L, char pal[]) { //verif, verif 2
+/*
+Nada que aclarar
+*/
+
+TListaL buscaL(TLista L, char pal[]) { //verif
     while(L && strcmp(pal,L->pal) > 0)
         L = L->sig;
-    return L && strcmp(pal,L->pal) == 0 ? L : NULL;
+    return (L && strcmp(pal,L->pal) == 0) ? L : NULL;
 }
 
-void elimina(TLista *L, char pal[]) { //verif, verif 2
+/*
+Nada que aclarar
+*/
+
+void elimina(TLista *L, char pal[]) { //verif
     TLista actL, antL;
     actL = *L;
     while(actL && strcmp(pal,actL->pal) != 0) {
         antL = actL;
         actL = actL->sig;
     }
-    if(actL) {
-        if(actL == *L)
+    if(actL && strcmp(pal,actL->pal) == 0) {
+        if(actL == *L) //unico o cabeza
             *L = (*L)->sig;
         else
-            antL->sig = actL->sig;
+            antL->sig = actL->sig; //otro
         free(actL);
     }
 }
 
-void eliminaPos(TLista *L, unsigned int pos) { //verif 2
+/*
+Los casos particulares a considerar son el unico/cabeza y otro
+*/
+
+void eliminaPos(TLista *L, unsigned int pos) { //verif
     TLista actL, antL;
     unsigned int aux = 0;
     actL = *L;
@@ -51,16 +63,16 @@ void eliminaPos(TLista *L, unsigned int pos) { //verif 2
     }
 }
 
-void eliminacionSucesiva(TLista *L, char pal[]) { //verif, verif 2
+void eliminacionSucesiva(TLista *L, char pal[]) { //verif
     TLista actL, antL, elim;
     actL = *L;
     while(actL) {
         if(strcmp(pal,actL->pal) == 0) {
             elim = actL;
-            if(actL == *L)
-                *L = (*L)->sig;
+            if(actL == *L) //Unico o cabeza
+                (*L) = (*L)->sig;
             else
-                antL->sig = actL->sig;
+                antL->sig = actL->sig; //otro
             actL = actL->sig;
             free(elim);
         }
@@ -71,7 +83,15 @@ void eliminacionSucesiva(TLista *L, char pal[]) { //verif, verif 2
     }
 }
 
-void insertaOrd(TLista *L, char pal[]) { //verif, verif 2
+/*
+Como es eliminacion sucesiva necesito la variable elim
+Si debo eliminar le asigno act al principio
+Si no elimino solo avanzo
+Si elimino, antes de eliminar avanzo y uso un solo free(elim) para todos los casos
+Los casos particulares a considerar son Unico/Cabeza y otro antes de la cabezay otro
+*/
+
+void insertaOrd(TLista *L, char pal[]) { //veri
     TLista actL, antL, new;
     new = (TLista) malloc(sizeof(nodo)); //(si corresponde)
     //asignar datos
@@ -87,9 +107,16 @@ void insertaOrd(TLista *L, char pal[]) { //verif, verif 2
         antL->sig = new;
 }
 
+/*
+Siempre lo primero que hago es asignarle el actual al siguiente del nuevo
+Si cabeza asigno el nuevo a L
+Si no es cabeza desenlazo el nodo solamente
+Los casos particulares a considerar son Lista Vacia/insercion antes de la cabezay otro
+*/
+
 /* === ALGORITMOS DE LISTA DOBLEMENTE ENLAZADA === */
 
-void muestraLD(TListaD LD) { //verif, verif 2
+void muestraLD(TListaD LD) { //verif
     Pnodo aux = LD.pri;
     while(aux) {
         printf("%c ", aux->dato);
@@ -97,33 +124,34 @@ void muestraLD(TListaD LD) { //verif, verif 2
     }
 }
 
-Pnodo busca(TListaD LD, char pal[]) { //verif 2
-
+Pnodo busca(TListaD LD, char pal[]) { //verif
     Pnodo actD = LD.pri;
     while(actD && strcmp(pal,actD->pal) > 0)
         actD = actD->sig;
-    return actD && strcmp(pal,actD->pal) == 0 ? actD : NULL;
+    return (actD && strcmp(pal,actD->pal) == 0) ? actD : NULL;
 }
-//ignorar
+
+//Aqui necesito una variable auxiliar no puedo recorrer con LD.pri directamente
+
 void eliminaLD(TListaD *LD, char pal[]) { //verif
     Pnodo aux = LD->pri;
     while(aux && strcmp(pal,aux->pal) > 0)
         aux = aux->sig;
     if(aux && strcmp(pal,aux->pal) == 0) {
-        if(LD->pri == LD->ult)
+        if(LD->pri == LD->ult) //unico
             LD->pri = LD->ult = NULL;
         else
             if(aux == LD->pri) {
-                LD->pri->sig->ant = NULL;
+                LD->pri->sig->ant = NULL; //cabeza pri
                 LD->pri = LD->pri->sig;
             }
         else
             if(aux == LD->ult) {
-                LD->ult->ant->sig = NULL;
+                LD->ult->ant->sig = NULL; //cabeza ult
                 LD->ult = LD->ult->ant;
             }
         else {
-            aux->ant->sig = aux->sig;
+            aux->ant->sig = aux->sig; //otro
             aux->sig->ant = aux->ant;
         }
         free(aux);
@@ -131,113 +159,50 @@ void eliminaLD(TListaD *LD, char pal[]) { //verif
 }
 
 /*
-void eliminaLD(TListaD *LD, char pal[]) { //verif
-    Pnodo aux = LD->pri;
-
-    if(LD->pri == LD->ult && strcmp(pal,LD->pri) == 0) {  //Lista doble con unico elem
-        LD->pri = NULL;
-        LD->ult = NULL;
-        free(aux);
-    }
-    else
-        if(strcmp(pal, LD->pri->pal) == 0) { //el elemento a eliminar es el que esta en la cabeza
-            LD->pri->sig->ant = NULL;
-            LD->pri = LD->pri->sig;
-            free(aux);
-        }
-    else
-        if(strcmp(pal, LD->ult->pal) == 0) { //el elemento a eliminar esta a lo ultimo
-            aux = LD->ult;
-            LD->ult->ant->sig = NULL;
-            LD->ult = LD->ult->ant;
-            free(aux);
-        }
-    else
-        while(aux && strcmp(pal,aux->pal) > 0) //otro
-            aux = aux->sig;
-
-    if(aux && strcmp(pal,aux->pal) == 0) {
-        aux->ant->sig = aux->sig;
-        aux->sig->ant = aux->ant;
-        free(aux);
-    }
-}
+En este caso como no es sucesiva solo necesito free(aux)
+Los casos particulares son Unico, Cabeza Pri, Cabeza Ult, Otro
 */
-//V3 - Controlar
-void eliminaLD(TListaD *LD, char pal[]) { //verif
-    Pnodo aux = LD->pri;
-    if(LD->pri)
-        if(strcmp(pal,LD->pri) == 0) {
-            if(LD->pri == LD->ult) { //unico elem
-                LD->pri = NULL;
-                LD->ult = NULL;
-            }
-<<<<<<< HEAD
-            else {                  //cabeza pri
-                LD->pri->sig->ant = NULL;
-                LD->pri = LD->pri->sig;
-            }
-            free(aux);
-        }
-        else
-            if(strcmp(pal, LD->ult->pal) == 0) { //cabeza ult
-                aux = LD->ult;
-                LD->ult->ant->sig = NULL;
-                LD->ult = LD->ult->ant;
-                free(aux);
-            }
-        else {
-            aux = aux->sig; //adelanto el primero
-            while(aux && strcmp(pal,aux->pal) > 0) //otro
-                aux = aux->sig;
 
-            if(aux && strcmp(pal,aux->pal) == 0) {
-                aux->ant->sig = aux->sig;
-                aux->sig->ant = aux->ant;
-                free(aux);
-            }
-        }
-=======
-            else
-                act = act->sig;
-Â Â Â Â Â Â Â Â }
->>>>>>> b0ed4979b318b7637fd7b975749877eb840737fc
-}
-
-void eliminaSucesiva(TListaD *LD) { //verif, verif 2
+void eliminaSucesiva(TListaD *LD) { //verif
     Pnodo act, elim;
-
-    act = LD->pri;
-    while(act) {
-        if(act->cantDias == 0) {
-            elim = act;
-            if(LD->pri == LD->ult) {
-                LD->pri = NULL;
-                LD->ult = NULL;
-            }
-            else
-                if(act == LD->pri) {
-                    LD->pri->sig->ant = NULL;
-                    LD->pri = LD->pri->sig;
+        act = LD->pri;
+        while(act != NULL) {
+            if(act->cantDias == 0) {
+                elim = act;
+                if(LD->pri == LD->ult) {
+                    LD->pri = NULL;
+                    LD->ult = NULL;
                 }
                 else
-                    if(act == LD->ult) {
-                        LD->ult->ant->sig = NULL;
-                        LD->ult = LD->ult->ant;
+                    if(act == LD->pri) {
+                        LD->pri->sig->ant = NULL;
+                        LD->pri = LD->pri->sig;
                     }
-                else {
-                    act->ant->sig = act->sig;
-                    act->sig->ant = act->ant;
-                }
-            act = act->sig;
-            free(elim);
-        }
-        else
-            act = act->sig;
-       }
+                    else
+                        if(act == LD->ult) {
+                            LD->ult->ant->sig = NULL;
+                            LD->ult = LD->ult->ant;
+                        }
+                    else {
+                        act->ant->sig = act->sig;
+                        act->sig->ant = act->ant;
+                    }
+                act = act->sig;
+                free(elim);
+            }
+            else
+                act = act->sig;
+        }
 }
 
-//ignorar
+/*
+En este caso como al ser sucesiva necesito elim
+Si pasa el if de que cumple la condicion para eliminar, ahi ya hago elim = act
+Si no tengo que eliminar va el else y solo hace aux = aux->sig;
+Luego de pasar todos los casos avanzo en aux y luego hago free (si corresponde eliminar)
+Los casos particulares son Unico, Cabeza Pri, Cabeza Ult, Otro
+*/
+
 void insertaOrd(TListaD *LD, char pal[]) { //verif
     Pnodo new, aux;
     new = (Pnodo) malloc(sizeof(nodoD));
@@ -245,7 +210,6 @@ void insertaOrd(TListaD *LD, char pal[]) { //verif
     aux = LD->pri;
     while(aux && strcmp(pal,aux->pal) > 0)
         aux = aux->sig;
-
     if(LD->pri == NULL) { //Lista vacia //no es valida la comparacion multiple en C
         new->sig = new->ant = NULL;
         LD->pri = LD->ult = new;
@@ -272,43 +236,14 @@ void insertaOrd(TListaD *LD, char pal[]) { //verif
         }
 }
 
-//V2 - Controlar
-void insertaOrd(TListaD *LD, char pal[]) { //verif
-    Pnodo new, aux;
-    new = (Pnodo) malloc(sizeof(nodoD));
-
-    if(LD->pri == NULL || strcmp(pal,LD->pri->pal) < 0) {
-        new->ant = NULL;
-        new->sig = LD->pri;
-        if(LD->pri == NULL)
-            LD->ult = new;
-        else
-            LD->pri->ant = new;
-
-        LD->pri = new;
-    }
-    else
-        if(strcmp(pal,LD->ult->pal) > 0) {
-            new->sig = NULL;
-            new->ant = LD->ult;
-            LD->ult->sig = new;
-            LD->ult = new;
-    }
-    else {
-        aux = LD->pri;
-        while(aux && strcmp(pal,aux->pal) > 0)
-            aux = aux->sig;
-
-        new->sig = aux;
-        new->ant = aux->ant;
-        aux->ant->sig = new;
-        aux->ant = new;
-    }
-}
+/*
+La diferencia entre la insercion y la eliminacion en listas circulares y dobles (porque en la simple da igual), es que la insercion debemos preguntar si es mayor o menor a la cabeza y no si es igual (como en la eliminacion)
+Los casos particulares son Lista Vacia, menor que cabeza pri, mayor que cabeza ult, otro
+*/
 
 /* === ALGORITMOS DE LISTA CIRCULAR === */
 
-void muestraLC(TListaC LC) { //verif, verif 2
+void muestraLC(TListaC LC) { //verif
     TListaC aux;
     if(LC) {
         aux = LC->sig;
@@ -319,17 +254,27 @@ void muestraLC(TListaC LC) { //verif, verif 2
     }
 }
 
-TListaC busca(TListaC LC, char pal[]) { //verif, verif 2
+/*
+Recordar que para mostrar en la lista circular conviene usar el do while
+Muestro
+Avanzo
+Condicion del while
+*/
+
+TListaC busca(TListaC LC, char pal[]) { //verif
     TListaC aux;
     if(LC) {
         aux = LC->sig;
         while(aux != LC && strcmp(pal,aux->pal) != 0)
             aux = aux->sig;
+        return (strcmp(pal, aux->pal) == 0) ? aux : NULL;
     }
-    return LC && strcmp(pal, aux->pal) == 0 ? aux : NULL;
+    else
+        return NULL;
 }
 
-//ignorar
+//En la busqueda de la lista circular como debo preguntar por LC al princpio tambien debo poner un else con un return NULL para cubrir todas las ramas
+
 void insertaLC(TListaC *LC, char pal[]) { //verif
     TListaC actC, antC, new;
     new = (TListaC) malloc(sizeof(nodoC)); //(si corresponde)
@@ -345,47 +290,26 @@ void insertaLC(TListaC *LC, char pal[]) { //verif
         if(strcmp(pal,(*LC)->pal) > 0) {//tengo que insertar en la cabeza
             new->sig = (*LC)->sig;
             (*LC)->sig = new;
-            *LC = new;
+             *LC = new;
         }
-        else {
+        else { //otro
             antC->sig = new;
             new->sig = actC;
         }
     }
-    else {
+    else { //vacia
         *LC = new;
         new->sig = new;
     }
 }
-//V2 - Controlar
-void insertaLC(TListaC *LC, char pal[]) { //verif
-    TListaC actC, antC, new;
-    new = (TListaC) malloc(sizeof(nodoC));
 
-    if(!*LC) {
-        *LC = new;
-        new->sig = new;
-    }
-    else
-        if(strcmp(pal,(*LC)->pal) > 0) {//tengo que insertar en la cabeza
-            new->sig = (*LC)->sig;
-            (*LC)->sig = new;
-            *LC = new;
-        }
-        else {
-            antC = (*LC);
-            actC = (*LC)->sig;
-            while(actC != *LC && strcmp(pal,actC->pal) > 0) {
-                antC = actC;
-                actC = actC->sig;
-            }
-            antC->sig = new;
-            new->sig = actC;
-        }
-}
+/*
+En la insercion en lista circular, tambien en la eliminacion, el anterior lo hago ult y el act lo hago el primero
+Dado que debo preguntar si LC es valido, si no se cumple va el else con el caso particular de que la lista venga vacia
+Los casos particulares son: Lista Vacia, Mayor a cabeza y Otro
+*/
 
-//lo vio bien
-void elimina(TListaC *LC, char pal[]) { //verif, verif 2
+void elimina(TListaC *LC, char pal[]) { //verif
     TListaC actC, antC;
     if(*LC) {
         antC = (*LC);
@@ -398,7 +322,7 @@ void elimina(TListaC *LC, char pal[]) { //verif, verif 2
             if(*LC == (*LC)->sig) //unico
                 *LC = NULL;
             else {
-                antC->sig = actC->sig; //desenlazo (vale para otro o cabeza)
+                antC->sig = actC->sig; //otro
                 if(actC == *LC) //cabeza
                     *LC = antC;
             }
@@ -407,11 +331,15 @@ void elimina(TListaC *LC, char pal[]) { //verif, verif 2
     }
 }
 
-//verif, lo vio bien
-void eliminaSucesiva(TListaC *LC, char pal[]) { //verif, verif 2
+/*
+Muy similar a la insercion
+Los casos particulares son: Unico, cabeza y otro
+*/
+
+void eliminaSucesiva(TListaC *LC, char pal[]) { //verif
     TListaC antC, actC, elim;
     if(*LC) {
-        antC = *LC;
+        antC = (*LC);
         actC = (*LC)->sig;
         do {
             if(strcmp(pal, actC->pal) == 0) {
@@ -419,7 +347,7 @@ void eliminaSucesiva(TListaC *LC, char pal[]) { //verif, verif 2
                 if(*LC == (*LC)->sig)   // unico nodo
                     *LC = NULL;
                 else {
-                    antC->sig = actC->sig; //desenlazar
+                    antC->sig = actC->sig; //otro
                     if(actC == *LC)  // cabeza
                         *LC = antC;
                     actC = actC->sig;
@@ -435,6 +363,20 @@ void eliminaSucesiva(TListaC *LC, char pal[]) { //verif, verif 2
 }
 //La ventaja del do while es q permite no repetir logica para el caso de la cabeza
 
+/*
+En la eliminacion multiple en la lista circular conviene usar do while para no repetir mucha logica
+El orden es:
+Empiezo por pri
+Pregunto si debo eliminar
+Si debo eliminar:
+-elim = act;
+-Casos particulares
+-Free
+La condicion del while es mientras LC sea valido y mientras actC sea distinto del primer elemento
+
+Los casos particulares son: unico, cabeza y otro
+*/
+
 int main() {
     TLista L;
     TListaD LD;
@@ -444,3 +386,4 @@ int main() {
     printf("Hello world!\n");
     return 0;
 }
+
